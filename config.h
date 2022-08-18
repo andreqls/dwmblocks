6f5607e365a65859743026467444fbc9091ede89
André Silva <andreqls@gmail.com>
2022-08-18 00:24:27 -0300

    Reordered icons

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*	{"",	"cpubars",	5,	17},*/
+	{"",	"battery",	60,	3},
 	{"",	"cputemp",	10,	18},
 	{"",	"memory",	10,	14},
 	{"",	"disk",		60,	5},
@@ -9,7 +10,6 @@ static const Block blocks[] = {
 	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 /*	{"",	"brightness",	0,	11},*/
-	{"",	"battery",	60,	3},
 	{"",	"clock",	60,	1},
 /*	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	{"",	"music",	0,	11},
